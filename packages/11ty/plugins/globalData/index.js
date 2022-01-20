2487a7d66d83115f0ce1921dd7ce7ef8c2c5533b
Hannah Balenda <hbalenda@getty.edu>
2022-01-19 16:40:26 -0800

    Refactor globalData plugin to load all yaml files in _data

--- packages/11ty/plugins/globalData/index.js
+++ packages/11ty/plugins/globalData/index.js
@@ -2,15 +2,17 @@ const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
 
+const dir = path.join('content', '_data')
+const files = fs.readdirSync(dir)
+
 const loadData = (fileName) => {
-  const filePath = path.join('content', '_data', fileName)
+  const filePath = path.join(dir, fileName)
   return yaml.load(fs.readFileSync(filePath))
 }
 
-const config = loadData('config.yaml')
-const figures = loadData('figures.yaml')
-const objects = loadData('objects.yaml')
-const publication = loadData('publication.yaml')
-const references = loadData('references.yaml')
-
-module.exports = { config, figures, objects, publication, references }
+module.exports = files.reduce((data, file) => {
+  const { ext, name } = path.parse(file)
+  if (!['.yml', '.yaml'].includes(ext)) return data
+  data[name] = loadData(file)
+  return data
+}, {})
